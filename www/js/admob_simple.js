48ae41c7222a8863a91f8e42aa3ebd0977ee9382
JITHIN SHAJI <jithinshaji1298@gmail.com>
2019-05-13 01:07:35 +0530

    tried admob plugin

---
+++ www/js/admob_simple.js
@@ -0,0 +1,5 @@
+// it will display smart banner at top center, using the default options
+if(AdMob) AdMob.createBanner({
+  adId: ca-app-pub-2328809934179418/4950889299.banner,
+  position: AdMob.AD_POSITION.TOP_CENTER,
+  autoShow: true });
